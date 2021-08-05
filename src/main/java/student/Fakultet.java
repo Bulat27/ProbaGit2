dc7658e3786e3e0a03b6d85089c9e5b0fd802d9e
Latbu <bulaat27@gmail.com>
2021-08-05 22:59:32 +0200

    Dodat fakultet

---
+++ src/main/java/student/Fakultet.java
@@ -0,0 +1,23 @@
+package student;
+
+public class Fakultet {
+
+    private int brojStudenata;
+    private int naziv;
+
+    public int getBrojStudenata() {
+        return brojStudenata;
+    }
+
+    public void setBrojStudenata(int brojStudenata) {
+        this.brojStudenata = brojStudenata;
+    }
+
+    public int getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(int naziv) {
+        this.naziv = naziv;
+    }
+}
