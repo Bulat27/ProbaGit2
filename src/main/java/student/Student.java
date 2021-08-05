21f5e70dc6ab15d4ae40029c8c1778b19911e531
Latbu <bulaat27@gmail.com>
2021-08-05 22:58:37 +0200

    Dodat student

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,23 @@
+package student;
+
+public class Student {
+
+    private String brojIndeksa;
+    private String imePrezime;
+
+    public String getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(String brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
+
+    public String getImePrezime() {
+        return imePrezime;
+    }
+
+    public void setImePrezime(String imePrezime) {
+        this.imePrezime = imePrezime;
+    }
+}
